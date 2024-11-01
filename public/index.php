446f455f5f56ae0423336799eb364698a1ba5a9a
Samuel Freitas <sfb.samuell@gmail.com>
2024-11-01 17:54:42 -0300

    atualização

--- public/index.php
+++ public/index.php
@@ -1,27 +1,38 @@
-<?php
+<?php 
+
 declare(strict_types=1);
 
-require_once __DIR__ . '/../Connection.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use aluraplay\Controller\{
+    Controller,
+    Error404controller,
+};
+use aluraplay\Persistence\Connection;
+use aluraplay\Repository\VideoRepository;
+
 $pdo = Connection::connect();
+$videoRepository = new VideoRepository($pdo);
 
-if(!array_key_exists('PATH_INFO', $_SERVER)|| $_SERVER['PATH_INFO'] === '/') {
-    require_once __DIR__ . '/../listagem-videos.php';
-} elseif ($_SERVER['PATH_INFO'] === '/novo-video') {
-    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-        require_once __DIR__ . '/../formulario.php';
-    } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        require_once __DIR__ . '/../novo-video.php';
-    }
+$routes = require_once __DIR__ . '/../config/routes.php';
 
-} elseif ($_SERVER['PATH_INFO'] === '/editar-video') {
-    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-        require_once __DIR__ . '/../formulario.php';
-    } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        require_once __DIR__ . '/../editar-video.php';
-    }
-}elseif ($_SERVER['PATH_INFO'] === '/remover-video') {
-    require_once __DIR__ . '/../remover-video.php';
-}else {
-    http_response_code(404);
+$pathInfo = $_SERVER['PATH_INFO']?? '/';
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+
+session_start();
+$isLoginRoute = $pathInfo === '/login';
+if(!array_key_exists('logado', $_SESSION) && !$isLoginRoute) {
+    header('Location: /login');
+    return;
 }
 
+$key = "$httpMethod|$pathInfo";
+if(key_exists($key, $routes)) {
+    $controllerClass = $routes["$httpMethod|$pathInfo"];
+
+    $controller = new $controllerClass($videoRepository);
+}else {
+    $controller = new Error404controller();
+}
+/** @var Controller $controller */
+$controller->processaRequisicao();
